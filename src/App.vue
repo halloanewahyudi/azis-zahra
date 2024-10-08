eb293833c68aa453b004acc0222d567ace61f2eb
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2024-10-08 11:47:29 +0700

    rev

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,9 @@ import HandVue from "./components/icons/Hand.vue";
 import HomeVue from "./components/icons/Home.vue";
 import LoveVue from "./components/icons/Love.vue";
 import TimeVue from "./components/icons/Time.vue";
+import {useRoute} from 'vue-router'
 
+const route = useRoute()
 </script>
 
 <template>
@@ -16,7 +18,7 @@ import TimeVue from "./components/icons/Time.vue";
     <div class="py-2 px-4 bg-primary text-secondary text-xl rounded-full flex items-center justify-center gap-4 w-full max-w-max mx-auto fixed left-0 right-0 bottom-2">
       <router-link to="/"> <HomeVue /> </router-link>
       <router-link to="/couple"> <LoveVue /> </router-link>
-      <div v-if="$route.path == '/couple' " class="flex items-center gap-5">
+      <div v-if="route.path == '/couple' " class="flex items-center gap-5">
         <a href="#waktu"> <TimeVue /> </a>
         <a href="#turut-mengundang"><HandVue /></a>
         <a href="#kondangan"><GiftVue /></a>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
-
 const routes = [
     { path: '/', component: () => import('./components/Cover.vue') },
     { path: '/couple', component: () => import('./components/Profile.vue') },
